cc6d223a32d50f3ddf48e674911917147beb79ee
Connor <resistingprawn5@gmail.com>
2024-05-02 22:32:12 -0600

    i think i got weather working, but it's in celsius so i need to write function to convert to fahrenheit

--- public/weather.js
+++ public/weather.js
@@ -1,21 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHER_API_KEY}')
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Missoula&appid=8d9db8e4bb0ce2768fc37dd27eb2be40')
         .then(response => response.json())
         .then(weatherData => {
+            
             const temperature = weatherData.main.temp;
             const description = weatherData.weather[0].description;
             const weatherContainer = document.getElementById('weather-container');
-
-            weatherContainer.innerHTML = `
-                <h2>Weather</h2>
+            weatherContainer.innerHTML = 
+                `<h2>Weather</h2>
                 <p>Temperature: ${temperature}°F</p>
-                <p>Description: ${description}</p>
-            `;
+                <p>Description: ${description}</p>`
+            ;
         })
         .catch(error => {
-            console.error('Error FETCHING the weather data:', error);
+            console.error('Error with fetching the weather data: ', error);
         
             const weatherContainer = document.getElementById('weather-container');
-            weatherContainer.innerHTML = `<p> ERROR LOADING WEATHER DATA</p>`;
+            weatherContainer.innerHTML = `<p>Error with loadng the weather data</p>`;
         });
+
+
+
+
 });
