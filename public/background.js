d9af994b1887eda1ed1720b1c3b356230dc2cf9d
Connor <resistingprawn5@gmail.com>
2024-05-04 15:54:51 -0600

    htmx button works but it just tints blue; would like to make it a dark mode button

--- public/background.js
+++ public/background.js
@@ -1,5 +1,21 @@
 function background() {
-    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Generate random color
-    document.body.style.backgroundColor = randomColor; // Set background color
+    var randomColor;
+    /* f (Math.random() > 0.5) { // Will generate a blue tint if greater than .5
+        var blueTint = Math.floor(Math.random() * 156) + 100;
+        randomColor = 'rgba(0, 0, ' + blueTint + ', 0.3)';
+    }
+    else {
+        var purpleTint = Math.floor(Math.random() * 156) + 100;
+        randomColor = 'rgba(' + purpleTint + ', 0, ' + purpleTint + ', 0.3)';
+    } */
+    
+
+    var blueTint = Math.floor(Math.random() * 156) + 100;
+    randomColor = 'rgba(0, 0, ' + blueTint + ', 0.3)';
+    ocument.body.style.backgroundColor = randomColor;
+
+
+    /* var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Generate random color
+    document.body.style.backgroundColor = randomColor; // Set background color */
 }
 background();
