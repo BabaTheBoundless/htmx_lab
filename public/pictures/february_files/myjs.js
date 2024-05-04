f82d57d6a140154186f0b64499572a5318c84c5c
Connor <resistingprawn5@gmail.com>
2024-05-03 21:17:36 -0600

    got dark mode working; need to change the colors of wrongmonth

--- public/pictures/february_files/myjs.js
+++ public/pictures/february_files/myjs.js
@@ -3,11 +3,9 @@ function toggleDarkMode() {
     var darkMode = element.classList.contains("dark-mode");
     if (darkMode) {
         element.classList.remove("dark-mode");
-        element.classList.add("light-mode");
     }
     else {
     element.classList.add("dark-mode");
-    element.classList.remove("light-mode");
 }
 }
 
